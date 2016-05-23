52f178b642ed25385c3a0df17d4e9724761dd7d3
felix <felix>
2016-05-23 11:38:29 +0800

    '商店信息抓取完成(档口页面+商店页面)'

--- spider/py/wd_cloth/wd_cloth/spiders/a17zwd.py
+++ spider/py/wd_cloth/wd_cloth/spiders/a17zwd.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy import log
+#from scrapy import log
 from wd_cloth.items import ShopItem
 from wd_cloth.items import ShopInfoItem
 import re
@@ -32,39 +32,38 @@ class A17zwdSpider(scrapy.Spider):
 		#匹配市场分页
 		m1=re.match(r"http://\w+\.17zwd\.com/market\.htm\?page=\d+",response.url)
 		#匹配店铺所有宝贝页+分页
-		m2=re.match(r"http://\w+\.17zwd\.com/shop/\d+\.htm\?item_type=onsale",response.url)
+		#m2=re.match(r"http://\w+\.17zwd\.com/shop/\d+\.htm\?item_type=onsale",response.url)
 		#匹配店铺所有宝贝页分页
-		m3=re.match(r"http://\w+\.17zwd\.com/shop/\d+\.htm\?item_type=onsale?page=\d+",response.url)
+		#m3=re.match(r"http://\w+\.17zwd\.com/shop/\d+\.htm\?item_type=onsale?page=\d+",response.url)
 		#匹配商品页
-		m4=re.match(r"http://\w+\.17zwd\.com/item\.htm\?GID=\d+",response.url)
+		#m4=re.match(r"http://\w+\.17zwd\.com/item\.htm\?GID=\d+",response.url)
 		
 		if m0:
-			if m1:
-				#市场分页信息。
-				print 'get markert page :%s' % response.url
-			else:
-				#市场首页信息。
-				print 'get markert index :%s' % response.url
-				yield self.parse_page(response)
+			if not m1:
+				#self.parse_page(response)
+				pageinfo = response.css('div.pageing').re('ZWDPager\(\'_pager\', (\d*), (\d*)\)')
+				if ( len(pageinfo) == 2 ) :
+					pagesize = int(pageinfo[0])
+					pagetotal = int(pageinfo[1])
+					pagecount = pagetotal / pagesize 
+					if ( pagetotal % pagesize != 0) : 
+						pagecount = pagecount+1
+					for i in range(pagecount):
+						if i > 0 :
+							page=i+1
+							url = response.urljoin('?page=%d' % page)
+							print 'get page:%s'% url
+							yield scrapy.Request(url,callback=self.parse)
+			
 			#分析市场信息，获取商店信息,获取商品列表url
 			for item in self.parse_market(response):
 				goodslisturl = item['shopurl'] + "?item_type=onsale"
+				#获取商店页面信息。
 				yield scrapy.Request(goodslisturl,callback=self.parse_shop)
 				yield item
-		elif m2:
-			if m3:
-				print 'get shop page: %s' % response.url
-			else:
-				print 'get shop index: %s' % response.url
-				#分析商店信息
-				yield self.parse_shop(response)
-				#分析分页信息
-				yield self.parse_page(response)
-			#抓取链接中增加：商品url
-			#for item in self.parse_goodslist(response):
-				#yield item
 
 
+	#分析市场页面内容(market.htm)
 	def parse_market(self, response):
 		shoplist = response.css('div.florid-ks-waterfall')
 		for index,shop in enumerate(shoplist):
@@ -78,6 +77,9 @@ class A17zwdSpider(scrapy.Spider):
 			if len(marketinfo) == 5 :
 				item['category'] = marketinfo[3]
 				item['tip'] = marketinfo[4]
+			else:
+				item['category'] = ''
+				item['tip'] = ''
 			contactinfo = shop.css('a.florid-arRight::attr(href)').extract()
 			item['qqinfo'] = contactinfo[0]
 			item['wwinfo'] = contactinfo[1]
@@ -86,6 +88,7 @@ class A17zwdSpider(scrapy.Spider):
 			self.log('shop info [%d]: name[%s],market[%s]'% args)
 			yield item
 	
+	#分析商店页面内容(shop/12345.htm)
 	def parse_shop(self,response):
 		#print 'parse_shop: %s' % response.url
 		item = ShopInfoItem()
@@ -102,8 +105,9 @@ class A17zwdSpider(scrapy.Spider):
 		#淘宝店地址
 		item['tburl']="http:" +response.css('div.florid-goods-details-taobao-enter a::attr(href)').extract_first()
 		
-		return item
+		yield item
 	
+	#分析商店页面内容(shop/12345.htm)，抓取商品信息
 	def parse_goodslist(self,response):
 		goodsurllist = response.css('div.florid-shop-link a::attr(href)').extract()
 		for a in goodsurllist:
@@ -112,28 +116,9 @@ class A17zwdSpider(scrapy.Spider):
 			#抓取商品信息页面
 			yield scrapy.Request(goodsurl,callback=self.parse_goods) 
 		
+	#分析商品页面内容(item.htm?gid=12345)
 	def parse_goods(self,response):
 		print 'get goods page: %s' % response.url
 		item = GoodsItem()
 		return item
-		
-	
-	def parse_page(self, response):
-		print 'get pageinfo'
-		pageinfo = response.css('div.pageing').re('ZWDPager\(\'_pager\', (\d*), (\d*)\)')
-		
-		if ( len(pageinfo) == 2 ) :
-			pagesize = int(pageinfo[0])
-			pagetotal = int(pageinfo[1])
-			pagecount = pagetotal / pagesize 
-			if ( pagetotal % pagesize != 0) : 
-				pagecount = pagecount+1
-			for i in range(pagecount):
-				if i > 0 :
-					page=i+1
-					url = response.urljoin('?page=%d' % page)
-					print 'get page:%s'% url
-					yield scrapy.Request(url,callback=self.parse_market)
-		else :
-			print 'no page found'
-			self.log('no pager fond!')
+
