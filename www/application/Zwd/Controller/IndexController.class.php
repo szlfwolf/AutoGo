01f663785cf370cd97931ccf1df71044ab42fd85
szlfwolf <szlfwolf@outlook.com>
2016-05-29 19:32:53 +0800

    add www

---
+++ www/application/Zwd/Controller/IndexController.class.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Zwd\Controller;
+use Common\Controller\HomebaseController;
+class IndexController extends HomebaseController{
+    public function index(){
+      echo "this is zwd index !";
+    }
+}
---
+++ www/data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'thinkcmf',
+    'DB_USER' => 'root',
+    'DB_PWD' => '',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'cmf_',
+    //密钥
+    "AUTHCODE" => 'Q1Ebk2q0PEGa1MLjoL',
+    //cookies
+    "COOKIE_PREFIX" => 'RDPYfd_',
+);
