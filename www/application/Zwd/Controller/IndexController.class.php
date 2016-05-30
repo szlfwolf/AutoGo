9db6745e72b62fafbf9f32b68f7e3243c2527c7a
szlfwolf <szlfwolf@outlook.com>
2016-05-30 22:23:38 +0800

    init theme zwdtheme

--- www/application/Zwd/Controller/IndexController.class.php
+++ www/application/Zwd/Controller/IndexController.class.php
@@ -3,8 +3,7 @@
 namespace Zwd\Controller;
 use Common\Controller\HomebaseController;
 class IndexController extends HomebaseController{
-    public function index(){
-      echo "this is zwd index!";
+    public function index(){    	
 	  $this->display(":index");
     }
 }
