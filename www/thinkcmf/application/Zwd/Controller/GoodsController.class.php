f58e42edc94f5345d7ff65e921fc12cb4b9e038e
szlfwolf <szlfwolf@outlook.com>
2016-06-06 22:55:45 +0800

    update goods theme

--- www/thinkcmf/application/Zwd/Controller/GoodsController.class.php
+++ www/thinkcmf/application/Zwd/Controller/GoodsController.class.php
@@ -13,17 +13,25 @@ class GoodsController extends HomebaseController{
 		$this->goodsinfo_model = M('Goodsinfo');
 		
 		$goodsid    =  I('get.gid',1); // 获取get变量
-		$where = array("id"=>$goodsid);
-		$goodsinfoArray = $this->goodsinfo_model->where($where)->select();
+		$where = array("s_goodsinfo.id"=>$goodsid);
+		$goodsinfoArray = $this->goodsinfo_model
+			->join('s_shop_goods on s_shop_goods.goodsid = s_goodsinfo.id')
+			->field('s_shop_goods.shopid,s_goodsinfo.*')
+			->where($where)->select();
 		$goodsinfo = $goodsinfoArray[0];
 		$goodsimgs = json_decode($goodsinfo['goodsimgs']);
 		#$goodsimgs = explode(",", $goodsinfo['goodsimgs']);
 		$goodsimgs_m = str_replace("50x50","400x400",$goodsimgs[0]);
+		
+		$shopwhere = array("id"=> $goodsinfo['shopid']);
+		$shopinfoArray = $this->shopinfo_model->where($shopwhere)->select();
+		$shopinfo = $shopinfoArray[0];
 		    	
 		#dump($goodsimgs);
 		$this->assign("goodsinfo",$goodsinfo);
 		$this->assign("goodsimgs",$goodsimgs);
 		$this->assign("goodsimgs_m",$goodsimgs_m);
+		$this->assign("shopinfo",$shopinfo);
 	  	$this->display(":goods");
     }
 }
