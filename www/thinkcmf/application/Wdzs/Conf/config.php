cf57b5f44e18dfb012b8497c33254a462a317c2a
szlfwolf <szlfwolf@outlook.com>
2016-08-08 00:05:19 +0800

    remove key

--- www/thinkcmf/application/Wdzs/Conf/config.php
+++ www/thinkcmf/application/Wdzs/Conf/config.php
@@ -2,8 +2,8 @@
 
 return array(
     'API_1688'	=> array(
-    	'APP_KEY' => 9982392,
-    	'APP_CODE'	=>'TEWwbczbnG5G',
+    	'APP_KEY' => '',
+    	'APP_CODE'	=>'',
     	'R_URL' => 'http://localhost/wdzs/index.php/index/token',
     	'API_BASE'			=> "http://gw.open.1688.com:80/openapi/",  
     	'API_ADD_PRODUCT'	=> "/param2/1/com.alibaba.product/alibaba.product.add/",	
