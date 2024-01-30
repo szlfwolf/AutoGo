8e98aaf5e9eb9dbc0b8b8a4e1e9f9f8e2f54a823
szlfwolf <szlfwolf@outlook.com>
2024-01-30 14:24:59 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         fuxi-code
+// @name         demo
 // @namespace    http://tampermonkey.net/
 // @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @match        https://fuxi.huawei.com/mine/codes/*
+// @match        https://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // ==/UserScript==
