44f89c106ac9bb14d1c7aa5e4258295de3a7d277
szlfwolf <szlfwolf@outlook.com>
2024-01-30 22:30:27 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,17 +1,17 @@
 // ==UserScript==
-// @name         demo
+// @name         宝安教育
 // @namespace    http://tampermonkey.net/
 // @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @match        https://*/*
+// @match        https://jxzl.baoan.edu.cn/student/home/schoolHome
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    const apiUrl = "/notification/user/";
+    const apiUrl = "https://jxzl.baoan.edu.cn/api/edu/ReportExam/GetStudentReportCard";
 
     // Your code here...
     const originOpen = XMLHttpRequest.prototype.open;
@@ -28,7 +28,66 @@
                     try {
                         const res = JSON.parse(result);
                         console.log(res)
-                        res[0].userName=res[0].userName+"-"+res[0].huaweiID
+                        const scoreTitle = {
+                            "Field": "Score",
+                            "Title": "得分",
+                            "IsPublic": false,
+                            "OtherTypeId": null,
+                            "SubjectIds": null,
+                            "Rowspan": 1,
+                            "Colspan": 1,
+                            "Sort": 2,
+                            "ChildrenTableHeaderList": null,
+                            "IsRange": true,
+                            "ParentField": null
+                        }
+                        const classRankingTitle = {
+                            "Field": "ClassRanking",
+                            "Title": "班级排名",
+                            "IsPublic": false,
+                            "OtherTypeId": null,
+                            "SubjectIds": null,
+                            "Rowspan": 1,
+                            "Colspan": 1,
+                            "Sort": 2,
+                            "ChildrenTableHeaderList": null,
+                            "IsRange": true,
+                            "ParentField": null
+                        }
+                        const schoolRankingTitle = {
+                            "Field": "SchoolRanking",
+                            "Title": "弘雅排名",
+                            "IsPublic": false,
+                            "OtherTypeId": null,
+                            "SubjectIds": null,
+                            "Rowspan": 1,
+                            "Colspan": 1,
+                            "Sort": 2,
+                            "ChildrenTableHeaderList": null,
+                            "IsRange": true,
+                            "ParentField": null
+                        }
+                        const rankingTitle = {
+                            "Field": "Ranking",
+                            "Title": "宝安排名",
+                            "IsPublic": false,
+                            "OtherTypeId": null,
+                            "SubjectIds": null,
+                            "Rowspan": 1,
+                            "Colspan": 1,
+                            "Sort": 2,
+                            "ChildrenTableHeaderList": null,
+                            "IsRange": true,
+                            "ParentField": null
+                        }
+                        res.Data.MySubjectScore.Headers.push(scoreTitle);
+                        res.Data.MySubjectScore.Headers.push(classRankingTitle);
+                        res.Data.MySubjectScore.Headers.push(schoolRankingTitle);
+                        res.Data.MySubjectScore.Headers.push(rankingTitle);
+//                         res.Data.MySubjectScore.Rows.forEach(function(subject) {
+//                             subject["Level "] = subject["Level "]+"/"+subject.Score+"/"+subject.ClassRanking+"/"+subject.SchoolRanking+"/"+subject.Ranking
+//                             console.log(subject);
+//                         });
                         return JSON.stringify(res);
                     } catch (e) {
                         return result;
